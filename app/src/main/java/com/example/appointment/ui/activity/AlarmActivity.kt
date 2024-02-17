124b5166e5ae2b3c5d2441721f8428c7cfa8ca20
kang <dlwprkdlq@naver.com>
2024-02-17 23:43:36 +0900

    - toast_layout 생성

---
+++ app/src/main/java/com/example/appointment/ui/activity/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appointment.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appointment.R
+
+class AlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
--- app/src/main/java/com/example/appointment/ui/activity/login/LoginActivity.kt
+++ app/src/main/java/com/example/appointment/ui/activity/login/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.appointment.ui.activity.login
 
 import android.content.Intent
+import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.example.appointment.R
@@ -46,7 +48,17 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
 
         loginViewModel.loginfail.observe(this){
             when(it){
-                0-> Toast.makeText(this,"아이디 와 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+                0->{val inflater = LayoutInflater.from(this)
+                    val layout = inflater.inflate(R.layout.toast_layout, null)
+                    val text = layout.findViewById<TextView>(R.id.text)
+                    text.text = "아이디 와 비밀번호를 입력하세요"
+
+                    val toast = Toast(this)
+                    toast.view = layout
+                    toast.duration = Toast.LENGTH_SHORT
+                    toast.show()}
+
+                //0-> Toast.makeText(this,"아이디 와 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
                 1-> Toast.makeText(this,"계정이 인증되지 않았습니다. 등록하신 이메일에서 계정을 인증해 주세요.", Toast.LENGTH_LONG).show()
                 2-> Toast.makeText(this,"계정이 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
                 3-> Toast.makeText(this,"비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
